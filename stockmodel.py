6351985dadc61df633571d0e3ca821eaeb72d2f1
Oliver H <honkmuc@kabelmail.de>
2021-02-23 10:52:36 +0100

    done env

---
+++ stockmodel.py
@@ -0,0 +1,16 @@
+import time
+from stable_baselines import PPO2
+
+def train_PPO(env_train, model_name, timesteps=50000):
+    """PPO model"""
+
+    start = time.time()
+    model = PPO2('MlpPolicy', env_train, ent_coef = 0.005, nminibatches = 8)
+    #model = PPO2('MlpPolicy', env_train, ent_coef = 0.005)
+
+    model.learn(total_timesteps=timesteps)
+    end = time.time()
+
+    model.save(f"{model_name}")
+    print('Training time (PPO): ', (end - start) / 60, ' minutes')
+    return model
